977fe5fb2fd53a0ec7e4818c824e060fea11168b
Zachary Perlmutter <zrp200@gmail.com>
2014-07-31 18:50:22 -0700

    Update and rename dictionary.rb to hangman.rb

--- lib/dictionary.rb
+++
@@ -1,1 +0,0 @@
-dictionary = %w array dog cat hammer army card beer queen zeal quest heist soldier program three ruby coffee cash dash splash crash flash
---
+++ lib/hangman.rb
@@ -0,0 +1,3 @@
+def term
+    %w(array dog cat hammer army card beer queen zeal quest heist soldier program three ruby coffee cash dash splash crash flash bash car plane jet zap taser gun rifle shotgun armour sword bow crossbow).sample
+end
