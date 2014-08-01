fbba858bb98394748f9c89d0a12a519b6959fff7
Zachary Perlmutter <zrp200@gmail.com>
2014-07-31 19:24:13 -0700

    Update hangman.rb

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,9 +1,14 @@
 
 class Hangman
-    attr_reader :actual, :display
-    # Initializes the data needed for the game 
+    # The word to be guessed
+        attr_reader :actual
+    # The mistake made while guessing. The game ends when @mistakes == 10
+        attr_reader :mistakes
+    # The letters already guessed. Helps prevent letters from being counted twice.
+        attr_reader :guessed
+    # Initializes the data needed for the game using parameter word as the word to be guessed
         def initialize(word=dictionary_sample)
-            @actual, @display = word, Array.new(@actual.length) {"_"}
+            @actual, @mistakes, @guessed, @display = word, 0, Array.new, Array.new(@actual.length) {"_"}
         end
     private
         # returns a random word from the array
