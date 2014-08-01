17088ae3f4cbec7a4e6540f021ac2cde212c3a9d
Zachary Perlmutter <zrp200@gmail.com>
2014-07-31 18:47:29 -0700

    Update dictionary.rb

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,1 +1,1 @@
-dictionary = %w array dog cat hammer army card beer queen zeal quest heist soldier program three ruby coffee cash dash splash crash
+dictionary = %w array dog cat hammer army card beer queen zeal quest heist soldier program three ruby coffee cash dash splash crash flash
