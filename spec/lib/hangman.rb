a24ebe7fe0c1484c6c9ea452f0f43a26bbfd0449
Zachary Perlmutter <zrp200@gmail.com>
2014-08-09 18:58:45 -0700

    Update hangman.rb

--- spec/lib/hangman.rb
+++ spec/lib/hangman.rb
@@ -1,2 +1,10 @@
 require 'hangman'
 require 'spec_helper.rb'
+describe Hangman do
+    hangman = Hangman.new
+    describe '#display' do
+        subject {hangman.display}
+        it "should be an array"
+        it "should have underscores"
+    end
+end
