7ff316b9764b0b1d63ead07ff13a66ebaa7f3c92
Zachary Perlmutter <zrp200@gmail.com>
2014-08-09 19:30:02 -0700

    Update hangman.rb

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,4 @@
-
+class GuessError < StandardError; end
 class Hangman
     # The word to be guessed
         attr_reader :actual
@@ -8,10 +8,31 @@ class Hangman
         attr_reader :guessed
     # This is what the user should see.
         attr_reader :display
-    # Initializes the data needed for the game using parameter word as the word to be guessed
+    # Initializes the data needed for the game using parameter word as the word to be guessed. Returns an new Hangman object
         def initialize(word=dictionary_sample)
             @actual, @mistakes, @guessed, @display = word, 0, Array.new, Array.new(@actual.length) {"_"}
         end
+        def check(guess)
+            if guess.length > 1
+                raise GuessError, "Guess needs to be one character"
+            elsif /[a-z]/.match(guess).nil?
+                raise GuessError, "Guess needs to be a letter"
+            elsif guessed.include? guess
+                puts "You already guessed that"
+            elsif actual.include? guess
+                i = 0
+                for letter in actual
+                    if letter == guess.downcase
+                        display[i] = letter
+                    end
+                    i += 1
+                end
+                guessed.push guess
+            else
+                
+            end
+        def incorrect(letter)
+            
     private
         # returns a random word from the array
             def dictionary_sample
